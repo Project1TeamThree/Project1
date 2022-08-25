9c882a7a8f73e99a2ea83854747de57967074681
Antartsant <107956474+Antartsant@users.noreply.github.com>
2022-08-24 21:47:23 -0500

    js file updates
    working on javascript files to link data to the content carrds

--- cheapShark.js
+++ cheapShark.js
@@ -1,49 +1,71 @@
 // console.log("hi");
 var requestUrlCheapShark = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15";
-var userInput
-var resultList = document.getElementById("results-area")
+var userInput;
+var resultList = document.getElementById("results-area");
+var appIDs;
+var testzone = document.getElementById('placeholder')
 
-requestUrlCheapShark = "https://www.cheapshark.com/api/1.0/games?title="
-// function getMarket(){
-//     fetch(requestUrl)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
+requestUrlCheapShark = "https://www.cheapshark.com/api/1.0/games?title=";
 
 function testFunction(){
     console.log(userInput)
 }
 
+
+
+
+
+function getAppIDs(title){
+    var appIDarrays = [];
+    fetch(requestUrlCheapShark + title + "&limit=10")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("CheapShark Data:", data)
+        // display(data)
+        for (let i=0; i < data.length-1 ; i++){
+            appIDarrays.push(data[i].gameID)
+        }
+        console.log("ID Array: ", appIDarrays);
+        getGameNames(appIDarrays);
+    });
+}
+
+function getGameNames(array){
+    var IDstring = array.toString();
+    console.log("Stringified Array:", IDstring)
+    fetch("https://www.cheapshark.com/api/1.0/games?ids=" + IDstring)
+    .then((response) => response.json())
+    .then((data) => {
+        display(data);
+    })
+}
+
 function display(data){
-    for (let i = 0; i < data.length; i++){
-        var newLi = document.createElement('li');
-        newLi.textContent = data[i].internalName;
-        resultList.append(newLi)
+    // for (let i = 0; i <= data.length; i++){
+    //     var newLi = document.createElement('li');
+    //     newLi.textContent = data[i].internalName;
+    //     resultList.append(newLi)
+    // }
+    var objectArray = []
+    console.log("User-friendly Data:", data)
+    for (x in data){
+        let gameData = data[x]
+        var gameListing = document.createElement('li');
+        gameListing.textContent = gameData.info.title;
+        // TODO: Figure out how/where/if to use the thumb image
+        // var gameIcon = document.createElement('img');
+        // gameIcon.setAttribute("href", gameData.info.thumb.value)
+        // testzone.append(gameIcon)
+        resultList.append(gameListing);
     }
+    
 }
-
-function getMarket (title){
-    fetch(requestUrlITAD + title)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("DATA1:", data)
-        display(data)
-    });
-}
-
-function getMarketTwo(title){
-    fetch(requestUrlCheapShark + title + "&limit=5")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("DATA2:", data)
-        display(data)
-    });
-}
-
 
 
 document.getElementById('searchButton').addEventListener('click', function(){
     userInput = document.getElementById("city").value;
-    getMarket(userInput);
-    getMarketTwo(userInput);
+    getAppIDs(userInput);
 })
+
+
+
